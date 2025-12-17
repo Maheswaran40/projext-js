0291d9c598bd32be695c56a093b05c089b6025b4
Azarudeen <azarudeen.b2003@gmail.com>
2025-12-17 16:58:33 +0530

    creating cartlist

--- Assets/Script/Products.js
+++ Assets/Script/Products.js
@@ -22,37 +22,8 @@ var products=[
         name:"Mobile4",
         price:19999,
         Image:"./Assets/Images/mobile.png"
-    },
-     {
-        id:4,
-        name:"Mobile4",
-        price:19999,
-        Image:"./Assets/Images/mobile.png"
-    },
-     {
-        id:4,
-        name:"Mobile4",
-        price:19999,
-        Image:"./Assets/Images/mobile.png"
-    },
-     {
-        id:4,
-        name:"Mobile4",
-        price:19999,
-        Image:"./Assets/Images/mobile.png"
-    },
-     {
-        id:4,
-        name:"Mobile4",
-        price:19999,
-        Image:"./Assets/Images/mobile.png"
-    },
-     {
-        id:4,
-        name:"Mobile4",
-        price:19999,
-        Image:"./Assets/Images/mobile.png"
-    },
+    }
+    
 ]
 
 function displayProducts(data){
@@ -69,7 +40,7 @@ function displayProducts(data){
                     <span>Price:${value.price}</span>
                 </div>
                 <div class="card-footer">
-                    <button class="btn btn-primary">Cart</button>
+                    <button class="btn btn-primary" onclick="addcart(${value.id})">Cart</button>
                 </div>
             </div>
         </div>`
@@ -77,3 +48,42 @@ function displayProducts(data){
     document.getElementById("display-data").innerHTML=getdata
 }
 displayProducts(products)
+
+
+var carlist=[]
+
+function addcart(productId){
+    var cartdata=products.find((value)=>value.id==productId)
+    var existing=carlist.find((value)=>value.id==productId)
+    console.log("Function is working");
+    
+    if(existing){
+        existing.quantity++
+    }
+    else{
+        carlist.push(cartdata)
+        cartdata.quantity=1
+
+    }
+
+    console.log(carlist);
+displayCartlist(carlist)
+    
+}
+// display cartlist
+
+function displayCartlist(data){
+    var cartdata=""
+    data.map((value)=>(
+        cartdata+=` 
+        <tr>
+                    <td><img src="${value.Image}" height="100px" width="100px"/></td>
+                    <td>${value.name}</td>
+                    <td>${value.price}</td>
+                    <td>${value.quantity}</td>
+                    <td><button class="btn btn-danger" onclick="removecart(${value.id})">Remove</button></td>
+        </tr>`
+    ))
+    document.getElementById("cart-list").innerHTML=cartdata
+
+}
